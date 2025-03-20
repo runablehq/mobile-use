ee773722c32523b36cf6ba731bce87dda5a1e672
Umesh Kumar <54642584+ua110110@users.noreply.github.com>
2025-03-20 17:14:49 +0530

    feat: Add more examples to use llms (#1)

--- examples/instagram_dm_claude.ts
+++ examples/instagram_dm_claude.ts
@@ -1,6 +1,7 @@
 import { mobileUse } from "@/src";
 
 async function main() {
+  // here we are using claude 3.5 sonnet
   const response = await mobileUse({
     task: "Open instagram and go to direct messages, send hi cutie to first person",
   });
